444ff352ee60b64bfba6e887c3697344835d25d6
Hakan Yilmaz <hakanbien@gmail.com>
2023-10-17 23:23:50 +0200

    17october2

--- src/components/word-generator/WordGenerator.js
+++ src/components/word-generator/WordGenerator.js
@@ -1,18 +1,28 @@
-// WordGenerator.js
+import { Document, Packer, Paragraph } from "docx";
 
 export const handleGenerateDocument = (data) => {
-    fetch('/template.txt')
+    fetch(process.env.PUBLIC_URL + '/template.txt')
         .then(response => response.text())
         .then(text => {
             const updatedText = text
-                .replace('{{adli}}', data.activeButton)
-                .replace('{{dosyaDurumu}}', data.fileStatus)
-                // Diğer değişiklikler...
+                .replace('{{adli}}', data.currentStatus)
+                .replace('{{dosyaDurumu}}', data.courtCity)
+                // Add other replacements as necessary...
 
-            const blob = new Blob([updatedText], { type: 'text/plain' });
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'guncellenmis-belge.docx';
-            link.click();
+            const doc = new Document({
+                sections: [
+                    {
+                        properties: {},
+                        children: [new Paragraph(updatedText)]
+                    }
+                ]
+            });
+
+            Packer.toBlob(doc).then(blob => {
+                const link = document.createElement('a');
+                link.href = URL.createObjectURL(blob);
+                link.download = 'guncellenmis-belge.docx';
+                link.click();
+            });
         });
-}
+};
